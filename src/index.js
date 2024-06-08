7539204ccece3ecaaa429634705882cc5a984832
Vincde <145802178+Vincde@users.noreply.github.com>
2024-06-08 14:49:08 +0200

    Add HashMap and set functions.
    The first function just creates the bucket. The second function is the hash(key) function. It turns the key into a hash used as index of the bucket array. Then the set(key,value) adds a new element. If the key is the same the node gets overwrited, if they are different a new node is created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-import "./style.css";
+const node = (key, value) => {
+  const next = null;
+  return { key, value, next };
+};
+
+const hashMap = () => {
+  const bucket = new Array(16).fill(undefined);
+
+  const hash = (key) => {
+    let hashCode = 0;
+    let res = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      res = hashCode % bucket.length;
+    }
+
+    return res;
+  };
+
+  const set = (key, value) => {
+    const newValue = value;
+    const newKey = key;
+    const hashKey = hash(key);
+    let pointer = bucket[hashKey];
+
+    if (pointer === undefined) {
+      pointer = node(newKey, newValue);
+      bucket[hashKey] = pointer;
+      return;
+    }
+
+    if (pointer.key === newKey) {
+      pointer.value = newValue;
+      return;
+    }
+
+    while (pointer.next !== null) {
+      pointer = pointer.next;
+      if (pointer.key === newKey) {
+        pointer.value = newValue;
+        return;
+      }
+    }
+
+    pointer.next = node(newKey, newValue);
+
+    pointer = 0;
+  };
+
+  return { bucket, hash, set };
+};
+
+const aa = hashMap();
+
+aa.set("ciao", "darling");
+aa.set("hello", "erere");
+aa.set("Hello", "trtr");
+console.log(aa);
